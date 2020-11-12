c6731e27e94597ea7be3559ce4b5a7a68ad4c93e
leoBakop <72123749+leoBakop@users.noreply.github.com>
2020-11-13 00:01:56 +0200

    Update kernel_proc.h

--- kernel_proc.h
+++ kernel_proc.h
@@ -123,7 +123,7 @@ Pid_t get_pid(PCB* pcb);
 
 /** @} */
 
-static inline void initialize_PTCB(PTCB* ptcb, PCB* pcb);  //was added by lui
+void initialize_PTCB(PTCB* ptcb, PCB* pcb);  //was added by lui
 void refcountIncr(PTCB* ptcb);                   //was added by lui
 void refcountDec(PTCB* ptcb);                    //was added by lui
 
